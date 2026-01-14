b9b95e2e531135bf48c9e5ab7395c152455ce3de
Felipe Peixoto <felipe.peixoto@stone.com.br>
2026-01-14 16:37:07 -0300

    Adicionando uso do terminal zsh forçadamente para o linux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.shell = "/usr/bin/zsh" -- ajuste se seu zsh estiver em outro path
+vim.opt.shellcmdflag = "-lc"
